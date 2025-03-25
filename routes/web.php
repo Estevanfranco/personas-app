ecc382be37e40c6d746c2d6fe71f6a17ca012667
Estevanfranco <90535714+Estevanfranco@users.noreply.github.com>
2025-03-25 10:39:23 -0500

    2copia

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\comunacontroller;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/comunas', [comunacontroller::class, 'index']);  
