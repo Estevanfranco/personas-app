08379206e7c1d424c52acdf6400ba77bb243596b
Estevanfranco <90535714+Estevanfranco@users.noreply.github.com>
2025-03-25 20:14:42 -0500

    commune add

--- resources/views/comuna/new.blade.php
+++ resources/views/comuna/new.blade.php
@@ -1,7 +1,6 @@
 <!doctype html>
 <html lang="en">
-  
-<head>
+  <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -9,36 +8,47 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>Listado de comunas</title>
+    <title>Add comuna</title>
   </head>
   <body>
-   <div class="container">
-    
-   <h1>Listado de comunas</h1>
-    <a href="{{route('comunas.create')}}" class="btn btn-succes">add</a>
-    <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">code</th>
-      <th scope="col">comune</th>
-      <th scope="col">municipality</th>
-      <th scope="col">action</th>
-    </tr>
-  </thead>
+   
+    <div class="container">
+    <h1>Add comuna</h1>
+    <form method="POST" action="{{route('comunas.store')}}">
+    @csrf
+  <div class="mb-3">
+    <label for="id" class="form-label">code</label>
+    <input type="text" class="form-control" id="id" aria-describedby="idlHelp" name="id"
+    disabled="disabled">
+    <div id="idHelp" class="form-text">comune code</div>
+  </div>
+  <div class="mb-3">
+  <label for="name" class="form-label">comune</label>
+    <input type="text" require class="form-control" id="name" aria-describedby="nameHelp" name="name"
+    placeholder="comune name.">
   
-  <tbody>
-  @foreach($comunas as $comuna)
-  <tr>
-      <th scope="row">{{$comuna ->comu_codi}}</th>
-      <td>{{$comuna ->comu_nomb}}</td>
-      <td>{{$comuna ->muni_nomb}}</td>
-      <td><span>action</span></td>
-    </tr>
-    @endforeach
+  </div>
+  
+  
+    <label for="municipality">municipality:</label>
+    <select class="form.select" id="municipality" name="code" required>
+      <option selected disabled value="">choose one..</option>
+      @foreach($municipios as $municipio)
+        <option value="{{$municipio->muni_codi}}">{{$municipio->muni_nomb}}</option>
+      @endforeach
+    </select>
     
   
-  </tbody>
-</table>
+
+  <div class="mt-3">
+    <button type="submit" class="btn btn-primary">Save</button>
+    <a href="{{route('comunas.index')}}" class="btn btn-warning">cancel</a>
+  </div>
+  
+</form>
+</div>
+    
+
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
@@ -49,6 +59,5 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
     -->
-   </div>
   </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/comunas', [comunacontroller::class, 'index']);  
+Route::get('/comunas', [comunacontroller::class, 'index']) -> name('comunas.index');  
+Route::post('/comunas', [comunacontroller::class, 'store'])-> name('comunas.store');  
+Route::get('/comunas/create', [comunacontroller::class, 'create'])-> name('comunas.create');  
